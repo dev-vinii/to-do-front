85323b3ec350269a2e72634b2ae2f164b1b2ed8f
Vinicius Luna <viniciusluna@yahoo.com>
2023-08-21 23:42:35 -0300

    add form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DogsComponent } from './dogs/dogs.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { DogsComponent } from './dogs/dogs.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dogs/dogs.component.ts
+++ src/app/dogs/dogs.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dogs',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class DogsComponent {
 
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      name: ['', Validators.required],
+      breed: ['', Validators.required],
+      owner: ['', Validators.required],
+      document: ['', Validators.required]
+    });
+
+  }
 }
